38050af0af12bf06327de7da44a90c3a2766400e
Cooper Garren <cgarren18@icloud.com>
2020-05-26 12:00:21 -0500

    fixed validation function

--- songdata.js
+++ songdata.js
@@ -17,7 +17,7 @@ $(function() {
 })
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
+function() {
     'use strict';
     window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -33,7 +33,7 @@ $(function() {
             }, false);
         });
     }, false);
-})();
+}
 
 function getParamsFromURL() {
     try {
