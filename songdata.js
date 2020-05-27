71fecf172900ab8f18937c96cd2a8e2359cbdc59
Cooper Garren <cgarren18@icloud.com>
2020-05-26 21:57:23 -0500

    updated funtion name

--- songdata.js
+++ songdata.js
@@ -17,7 +17,7 @@ $(function() {
 })
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-function() {
+function validateField() {
     'use strict';
     window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
