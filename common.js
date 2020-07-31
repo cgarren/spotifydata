5b89fe60b1d258491fb503da4982337f874bbe0c
Cooper Garren <cgarren18@icloud.com>
2020-07-31 00:33:09 -0500

    .com

--- common.js
+++ common.js
@@ -71,7 +71,7 @@ $.get('nav.html', function(data){
     $('body').prepend(data);
     if (localStorage.getItem('raw_hash') == null || localStorage.getItem('raw_hash') == "") {
         console.log("NULL !")
-        $("#songdata_link")[0].href = "https://spotifydata.ml/songdata"
+        $("#songdata_link")[0].href = "https://spotifydata.com/songdata"
         $("#userdata_dropdown a:nth-child(1)")[0].href = "https://spotifydata.com/profile"
         $("#userdata_dropdown a:nth-child(2)").hide()//[0].href = "https://spotifydata.ml/profile"
         $("#userdata_dropdown a:nth-child(3)").hide()//[0].href = "https://spotifydata.ml/profile"
@@ -79,7 +79,7 @@ $.get('nav.html', function(data){
         $("#userdata_dropdown a:nth-child(5)").hide()//[0].href = "https://spotifydata.ml/profile"
         $("#userdata_dropdown a:nth-child(6)").hide()//[0].href = "https://spotifydata.ml/profile"
     } else {
-        $("#songdata_link")[0].href = "https://spotifydata.ml/songdata" + localStorage.getItem('raw_hash')
+        $("#songdata_link")[0].href = "https://spotifydata.com/songdata" + localStorage.getItem('raw_hash')
         $("#userdata_dropdown a:nth-child(1)")[0].href = "https://spotifydata.com/profile" + localStorage.getItem('raw_hash')
         $("#userdata_dropdown a:nth-child(2)").hide()//[0].href = "https://spotifydata.ml/profile" + localStorage.getItem('raw_hash')
         $("#userdata_dropdown a:nth-child(3)").hide()//[0].href = "https://spotifydata.ml/profile" + localStorage.getItem('raw_hash')
